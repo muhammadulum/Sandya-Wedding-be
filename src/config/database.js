9836d8e129d311752d4320d29af1a1ca4c363b83
muhammadulum <muhammadmulum99@gmail.com>
2026-08-13 06:36:54 +0700

    a

--- src/config/database.js
+++ src/config/database.js
@@ -5,8 +5,8 @@ dotenv.config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  // process.env.DB_PASSWORD,
-  "postgres",
+  process.env.DB_PASSWORD,
+  //"postgres",
   {
     host: process.env.DB_HOST,
     dialect: "postgres",
